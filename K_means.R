7575f2a0112c8bd51733fe91da27b7dd7550a457
mnsekh111 <mnsekh111@gmail.com>
2016-02-12 16:31:48 -0500

    Added comments

--- K_means.R
+++ K_means.R
@@ -1,15 +1,24 @@
-#Read the data file
+# Read the data file
 dataset = read.csv("~/GitHub/ALDA/hw2-data.csv");
 
-#View the dataset
+# View the dataset
 View(dataset);
+
+# Refining the dataset
 dataset.features = dataset;
+
+# Removing unncessary attributes
 dataset.features$Id <- NULL;
 
 for (i in 2:6) {
   result <- kmeans(dataset.features,i);
+  
+  # output file
   jpeg(file = paste("~/GitHub/ALDA/k_means_",i,".jpeg",sep = ""));
+  
+  # Generate plot
   plot(dataset.features,col = result$cluster);
+  
   dev.off();
 }
 
